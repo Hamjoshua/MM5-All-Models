ffa3c348da3390588ac17be6127f69f12d6cef09
Hamjoshua <72654119+Hamjoshua@users.noreply.github.com>
2025-10-10 08:47:14 +0700

    Работающая функция для парсинга матем. функций

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Checked')
+from sympy import symbols, sympify
+from sympy.utilities.lambdify import lambdify
+
+def safe_parse_function(func_str, n_vars):
+    vars = symbols(' '.join([f'x{i+1}' for i in range(n_vars)]))
+    expr = sympify(func_str)
+    f = lambdify(vars, expr, modules='numpy')
+    return lambda x: f(*x)
+
+if __name__ == "__main__":
+    f = safe_parse_function("x1**2 + (x2 - 3)**2", 2)
+    result = f([1, 2])
+    print(result)
