4078db3207bf429cebd3a2e067d6827030b6e835
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-10-30 19:37:05 +0700

    Метод случайного поиска

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def safe_parse_function(func_str, n_vars):
     return lambda x: f(*x)
 
 def hook_jeeves(func_str, x0, deltas=None, epsilon=1e-3, alpha=0.5, max_iter=1000):
+    """
+    Статус: зачтено
+    """
     f = safe_parse_function(func_str, 2)
     x = np.array(x0, dtype=float)
     n = len(x)
@@ -55,11 +58,40 @@ def hook_jeeves(func_str, x0, deltas=None, epsilon=1e-3, alpha=0.5, max_iter=100
     
     return x, f(x)
 
-# === Пример использования ===
+def random_search(func_str, bounds, max_iter=1000):
+    """
+    Статус: не зачтено
+    """
+    n_vars = len(bounds)
+    f = safe_parse_function(func_str, n_vars)
+    
+    best_x = None
+    best_val = float('inf')
+    
+    for i in range(max_iter):
+        x = np.array([np.random.uniform(low, high) for (low, high) in bounds])
+        val = f(x)
+        if val < best_val:
+            print(f'Лучшее значение для х={x}: {val}, итерация №{i}')
+            best_val = val
+            best_x = x       
+            
+    
+    return best_x, best_val
+
+# # === Пример использования ===
+# if __name__ == "__main__":
+#     func = "(x1-2)^2"   # минимум в (0, 3)
+#     print(func)
+#     x0 = [1.0, 1.0]                # начальная точка
+#     result, f_val = hook_jeeves(func, x0, epsilon=1e-4)
+#     print(f"Найден минимум в: {result}")
+#     print(f"Значение функции: {f_val:.6f}")
+
+# === Случайный поиск (это все временно, потом будет нормальный консоль-запускатор)
 if __name__ == "__main__":
-    func = "(x1-2)^2"   # минимум в (0, 3)
-    print(func)
-    x0 = [1.0, 1.0]                # начальная точка
-    result, f_val = hook_jeeves(func, x0, epsilon=1e-4)
+    func = "(x1-2)**2 + (x2-3)**2"  # минимум: (2,3)
+    bounds = [(-10, 10), (-10, 10)]
+    result, f_val = random_search(func, bounds, max_iter=10000)
     print(f"Найден минимум в: {result}")
     print(f"Значение функции: {f_val:.6f}")
