f1b808d63329d6241c1056789dc5f416f7513b91
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-11-14 08:49:41 +0700

    Добавлен метод дихотомии

--- methods.py
+++ methods.py
@@ -26,6 +26,39 @@ class AbstractMethod:
         return lambda x: f(*x)
 
 
+class DichotomyMethod:
+    def __init__(self):
+        self.name = "Метод дихотомии"
+    
+    def minimize(self, func, a, b, epsilon=1e-6, delta=1e-6, max_iter=1000):
+        """
+        func - минимизируемая функция одной переменной
+        a, b - начальный интервал поиска минимума
+        epsilon - малое число для смещения точек в интервале
+        delta - точность по длине интервала
+        max_iter - максимальное число итераций
+        
+        Возвращает точку минимума на отрезке [a,b].
+        """
+        
+        k = 0
+        while (b - a) / 2.0 > delta and k < max_iter:
+            y = (a + b - epsilon) / 2.0
+            z = (a + b + epsilon) / 2.0
+            f_y = func(y)
+            f_z = func(z)
+            
+            if f_y <= f_z:
+                b = z
+            else:
+                a = y
+            
+            k += 1
+        
+        x_min = (a + b) / 2.0
+        return x_min
+
+
 class GradientDescentMethod(AbstractMethod):
     def __init__(self):
         super().__init__()
